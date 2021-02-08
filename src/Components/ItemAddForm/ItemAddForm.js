2962d3df6b5a5dc67f475e227be28cddff8f0e41
YaroslavKutsmyda <kutsmydadev@gmail.com>
2021-02-08 16:18:41 +0200

    Create form input and submit

--- src/Components/ItemAddForm/ItemAddForm.js
+++ src/Components/ItemAddForm/ItemAddForm.js
@@ -4,13 +4,36 @@ import './ItemAddForm.css'
 
 class ItemAddForm extends Component {
 
+    state={
+        label: ''
+    }
+
+    onLabelChange = (ev)=>{
+
+       this.setState({
+           label: ev.target.value
+       })
+    }
+
+    onSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.addItem(this.state.label)
+    }
+
 
     render() {
-        const {addItem} = this.props
+
         return (
-            <div className='ItemAddForm'>
-                <button onClick={()=>addItem('hgf6tgh')} className='btn btn-outline-secondary'>Add Item</button>
-            </div>
+            <form className="ItemAddForm d-flex"
+            onSubmit={this.onSubmit}>
+                <input type='text'
+                className="form-control"
+                onChange={this.onLabelChange}
+                placeholder='What i Need TODO'
+                value={this.state.label}/>
+                <button className='btn btn-outline-secondary color'>Add </button>
+        </form>
+
         );
     }
 }
